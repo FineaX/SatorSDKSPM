3687afa806d35b261d5e62f1ce16dcf82cc05937
FineaX <comsubins@gmail.com>
2024-06-23 18:09:45 +0800

    a

--- Package.swift
+++ Package.swift
@@ -1,20 +1,29 @@
 // swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
+//.package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0"),
+//.package(url: "https://github.com/CoderMJLee/MJRefresh.git", from: "3.7.1"),
+//.package(url: "https://github.com/jdg/MBProgressHUD.git", .upToNextMajor(from: "1.2.0"))
 import PackageDescription
 
 let package = Package(
     name: "SatorSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SatorSDK",
-            targets: ["SatorSDK"])
+        .library(name: "SatorSDK", targets: ["SatorSDK"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0"),
+        .package(url: "https://github.com/CoderMJLee/MJRefresh.git", from: "3.7.1"),
+        .package(url: "https://github.com/jdg/MBProgressHUD.git", .upToNextMajor(from: "1.2.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SatorSDK")
+        .binaryTarget(
+            name: "SatorSDK",
+            url:"https://github.com/FineaX/SatorSDKDemo/releases/download/1.0.1/SatorSDK.xcframework.zip",
+            checksum: "b9a7139cd27f13c135beaabf915f364d094041a58785d51c3032a394c0651b5e"
+        )
     ]
 )
+
