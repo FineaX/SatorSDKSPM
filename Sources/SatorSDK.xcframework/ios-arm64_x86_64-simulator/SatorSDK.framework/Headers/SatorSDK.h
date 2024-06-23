0fdca7abfb7d8d5b3b38611846016a743e543fde
FineaX <comsubins@gmail.com>
2024-06-23 22:19:23 +0800

    11

---
+++ Sources/SatorSDK.xcframework/ios-arm64_x86_64-simulator/SatorSDK.framework/Headers/SatorSDK.h
@@ -0,0 +1,18 @@
+//
+//  SatorSDK.h
+//  SatorSDK
+//
+//  Created by mac on 2024/6/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SatorSDK.
+FOUNDATION_EXPORT double SatorSDKVersionNumber;
+
+//! Project version string for SatorSDK.
+FOUNDATION_EXPORT const unsigned char SatorSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SatorSDK/PublicHeader.h>
+
+
