632ad6e98c9cf970ed75c5cad51f255eaabc72f3
FineaX <comsubins@gmail.com>
2024-06-23 19:33:24 +0800

    a

--- Package.swift
+++ Package.swift
@@ -1,17 +1,15 @@
 // swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-//.package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0"),
-//.package(url: "https://github.com/CoderMJLee/MJRefresh.git", from: "3.7.1"),
-//.package(url: "https://github.com/jdg/MBProgressHUD.git", .upToNextMajor(from: "1.2.0"))
+
 import PackageDescription
 
 let package = Package(
     name: "SatorSDKSPM",
-    platforms: [
-        .iOS(.v13)
-    ],
     products: [
-        .library(name: "SatorSDKSPM", targets: ["SatorSDKSPM"]),
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SatorSDKSPM",
+            targets: ["SatorSDKSPM"]),
     ],
     dependencies: [
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0"),
@@ -28,6 +26,6 @@ let package = Package(
                 .product(name: "MBProgressHUD", package: "MBProgressHUD"),
             
             ]),
-        .binaryTarget(name:"SatorSDK", path: "SatorSDK.xcframework")
+        .binaryTarget(name:"SatorSDK", path: "./Sources/SatorSDK.xcframework")
     ]
 )
