37ba900554a5ee3779af986cc2eb61430a360d84
FineaX <comsubins@gmail.com>
2024-06-23 18:00:03 +0800

    1

--- Package.swift
+++ Package.swift
@@ -9,12 +9,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SatorSDK",
-            targets: ["SatorSDK"]),
+            targets: ["SatorSDK"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SatorSDK"),
+            name: "SatorSDK")
     ]
 )
